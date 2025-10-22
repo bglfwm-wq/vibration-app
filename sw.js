b32ddce9a77a1574032e582179540f4a3c5500b6
bglfwm-wq <bglfwm@gmail.com>
2025-10-21 20:33:37 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,32 @@
-[Immersive content redacted for brevity.]
+self.addEventListener('message', event => {
+    const data = event.data;
+
+    if (data && data.type === 'vibrate') {
+        const promise = self.registration.showNotification('¡Vibración!', {
+            vibrate: data.pattern 
+        });
+
+        promise.then(() => {
+            replyToClients({ type: 'vibration_reply', success: true });
+        }).catch(err => {
+            replyToClients({ type: 'vibration_reply', success: false, error: err.message });
+        });
+    }
+});
+
+function replyToClients(message) {
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true })
+        .then(clientList => {
+            for (const client of clientList) {
+                client.postMessage(message);
+            }
+        });
+}
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
