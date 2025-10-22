b1f806fee442d6793a7ce652db5a8f18059b3f2d
bglfwm-wq <bglfwm@gmail.com>
2025-10-21 20:17:13 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,13 @@
 self.addEventListener('message', event => {
-    // Escucha el mensaje 'vibrate' del index.html
     if (event.data === 'vibrate') {
         
-        // --- ¡AQUÍ ESTÁ TU NUEVO PATRÓN! ---
         // Patrón largo de Halloween: (dum-dum... duuummm... ... DUUUUUUUUM)
         const vibrationPattern = [150, 100, 150, 300, 800, 500, 1500];
-        // ---------------------------------
 
-        // Muestra la notificación para activar la vibración
         const promise = self.registration.showNotification('¡Es tu turno!', {
             vibrate: vibrationPattern
         });
 
-        // Envía una respuesta de vuelta al index.html
         promise.then(() => {
             replyToClients({ type: 'vibration_reply', success: true });
         }).catch(err => {
@@ -21,7 +16,6 @@ self.addEventListener('message', event => {
     }
 });
 
-// Función para responder al index.html
 function replyToClients(message) {
     self.clients.matchAll({ type: 'window', includeUncontrolled: true })
         .then(clientList => {
@@ -31,7 +25,6 @@ function replyToClients(message) {
         });
 }
 
-// Código estándar del Service Worker para que se active rápido
 self.addEventListener('install', event => {
     self.skipWaiting();
 });
