cb10df9d2ca34c4a895948463019d9cf45107ea9
bglfwm-wq <bglfwm@gmail.com>
2025-10-21 19:28:04 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,34 @@
 self.addEventListener('message', event => {
-  if (event.data === 'vibrate') {
-    self.registration.showNotification('Vibración', {
-      vibrate: [200, 100, 200, 100, 300],
-      silent: true
-    });
-  }
+    if (event.data === 'vibrate') {
+        const promise = self.registration.showNotification('Vibración', {
+            vibrate: [200, 100, 300, 100, 200],
+            silent: true,
+            tag: 'vibration-test'
+        });
+
+        promise.then(() => {
+            replyToClients({ type: 'vibration_reply', success: true });
+        }).catch(err => {
+            replyToClients({ type: 'vibration_reply', success: false, error: err.message });
+        });
+    }
 });
 
+function replyToClients(message) {
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true })
+        .then(clientList => {
+            for (const client of clientList) {
+                client.postMessage(message);
+            }
+        });
+}
+
 self.addEventListener('install', event => {
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+    event.waitUntil(self.clients.claim());
 });
+
+
